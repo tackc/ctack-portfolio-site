1d4df4ab38142a33214a92729d4e08417bcd6875
Chris Tack <chris_tack@mac.com>
2018-12-26 20:56:03 -0800

    getting resume to display, download, and print

--- src/pages/ScrollingModal.jsx
+++ src/pages/ScrollingModal.jsx
@@ -1,18 +1,35 @@
-import React from 'react'
+import React from 'react';
 import { Button, Header, Icon, Image, Modal } from 'semantic-ui-react'
 import Resume from './Resume';
 
-const ScrollingModal = () => (
-    <Modal trigger={<Button>View my résumé</Button>}>
-        <Modal.Content>
-            <Resume/>
-        </Modal.Content>
-        <Modal.Actions>
-        <Button primary>
-            Proceed <Icon name='right chevron' />
-        </Button>
-        </Modal.Actions>
-    </Modal>
-)
+class ScrollingModal extends React.Component {
+    handlePrint = () => {
+        var myWindow = window.open("", "Resume");
+        myWindow.document.write(`<iframe
+            src="https://drive.google.com/file/d/1lxpQB5QlDa1H3cV4kxS0ejY1hGcExX6q/preview",
+        />`);
+        myWindow.document.close();
+        myWindow.focus();
+        myWindow.print();
+        myWindow.close();
+    }
+    render() {
+        return(
+            <Modal trigger={<Button>View my résumé</Button>}>
+                <Modal.Content>
+                    <Resume handlePrint={this.handlePrint} />
+                </Modal.Content>
+                <Modal.Actions>
+                    <Button href="https://dl.dropboxusercontent.com/s/vx7c2g4dngd4mpm/Chris_Tack_Resume.pdf?dl=0">
+                        Download &nbsp; &nbsp;<Icon name='download' />
+                    </Button>
+                    <Button onClick={this.handlePrint}>
+                        Print &nbsp;<Icon name='print' />
+                    </Button>
+                </Modal.Actions>
+            </Modal>
+        )
+    }
+}
 
-export default ScrollingModal
+export default ScrollingModal;
