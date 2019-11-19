9e7f59b1207aeb0f6ec4231a451452dfccd5d6e2
Chris Tack <chris_tack@mac.com>
2019-11-19 12:00:40 -0800

    Updates

---
+++ precache-manifest.e33af8245aefa223740c3336cae19348.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e34749369a55bfb3153e4791f569fc61",
+    "url": "/ctack-portfolio-site/index.html"
+  },
+  {
+    "revision": "6c3c73952d88376bc7c5",
+    "url": "/ctack-portfolio-site/static/css/main.a754f243.chunk.css"
+  },
+  {
+    "revision": "d7a5d07934f545eed514",
+    "url": "/ctack-portfolio-site/static/js/2.5346d288.chunk.js"
+  },
+  {
+    "revision": "6c3c73952d88376bc7c5",
+    "url": "/ctack-portfolio-site/static/js/main.d490dd20.chunk.js"
+  },
+  {
+    "revision": "5efb269e05051314a1da",
+    "url": "/ctack-portfolio-site/static/js/runtime-main.c0b5b48a.js"
+  },
+  {
+    "revision": "64b13ae2b408e94b4a38ec32c3ef2d99",
+    "url": "/ctack-portfolio-site/static/media/ChrisTack.64b13ae2.png"
+  },
+  {
+    "revision": "f3b996c32eacdaad324f689a75f88bc9",
+    "url": "/ctack-portfolio-site/static/media/brand-color.f3b996c3.png"
+  },
+  {
+    "revision": "9fe2baccc763d48618459fe91636e7e6",
+    "url": "/ctack-portfolio-site/static/media/headshot.9fe2bacc.jpg"
+  }
+]);
