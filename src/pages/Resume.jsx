2c61e5c1b83a5fde641f0bb573f092341815bad2
Chris Tack <chris_tack@mac.com>
2019-11-20 08:20:12 -0800

    Remove unused code

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -2,21 +2,6 @@ import React from 'react';
 // import { Document, Page } from 'react-pdf';
 
 class Resume extends React.Component {
-    // constructor(props) {
-    //     super(props)
-    //     this.handlePrint = this.handlePrint.bind(this)
-    // }
-    // handlePrint = () => {
-    //     var myWindow = window.open("", "Resume", "width=800, height=600");
-    //     myWindow.document.write(`<iframe
-    //         src="https://drive.google.com/file/d/1lxpQB5QlDa1H3cV4kxS0ejY1hGcExX6q/preview",
-    //     />`);
-    //     myWindow.document.close();
-    //     myWindow.focus();
-    //     myWindow.print();
-    //     myWindow.close();
-    //     };
-    
         render() {
         return (
             <div className="Resume">
