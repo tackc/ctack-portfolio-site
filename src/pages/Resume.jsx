20891b686a2b7e086191eb9e74d5c51f3d8c3f96
Chris Tack <chris_tack@mac.com>
2020-02-12 17:37:26 -0800

    Work on updating resume

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -3,17 +3,22 @@ import React from 'react';
 
 class Resume extends React.Component {
         render() {
-        return (
-            <div className="Resume">
-                <iframe
-                className="doc"
-                title="Chris Tack Resume"
-                src="https://drive.google.com/file/d/1lxpQB5QlDa1H3cV4kxS0ejY1hGcExX6q/preview" 
-                width="80%" 
-                height="800"
-                />
-            </div>
-        );
+            // return (
+            //     <div className="Resume">
+            //         <iframe
+            //         className="doc"
+            //         title="Chris Tack Resume"
+            //         src="https://drive.google.com/file/d/1lxpQB5QlDa1H3cV4kxS0ejY1hGcExX6q/preview" 
+            //         width="80%" 
+            //         height="800"
+            //         />
+            //     </div>
+            // );
+            return (
+                <div className="Resume">
+                    <iframe title="chrisTackResume" src="https://tackc.github.io/resume-template/" frameborder="0"></iframe>
+                </div>
+            );
         }
     }
 
